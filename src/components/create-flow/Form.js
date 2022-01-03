9b869ae6929b1a8f98a78fdc94c63caeb7f91e1d
TimNguyen21 <tim.nguyen2112@gmail.com>
2022-01-03 15:21:55 -0800

    Added addition input fields in create flow form

--- src/components/create-flow/Form.js
+++ src/components/create-flow/Form.js
@@ -11,8 +11,9 @@ const Form = () => {
             </header>
             <form class="create-flow-form__form">
                 <label class="create-flow-form__header-label">BUILD YOUR PETITION</label>
-                <div class="create-flow-form__input">
-                    <div class="create-flow-form__input-label">
+                {/*Edit Image Section*/}
+                <div class="create-flow-form__input-image">
+                    <div class="create-flow-form__input-image-label">
                         <div class="create-flow-form__input-label-text">
                             <div class="create-flow-form__input-label-icon">&#128247;</div>
                             CHANGE IMAGE
@@ -22,6 +23,62 @@ const Form = () => {
                         </div>
                     </div>
                     <img src="https://media1.popsugar-assets.com/files/thumbor/PjjJ4I0zydAEM56U4ngo9GMYPFY/1x647:2001x1471/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/09/23/671/n/1922243/9149b6bd5d88df88e30c16.96885837_.jpg" class="create-flow-form__input-image-area"></img>
+                </div>
+                {/*Edit Title Section*/}
+                <div className="create-flow-form__input">
+                    <div className="create-flow-form__input-label">
+                        <div className="create-flow-form__input-label-text">
+                            <div className="create-flow-form__input-label-icon">&#9998;</div>
+                            EDIT TITLE
+                        </div>
+                        <div className="create-flow-form__input-tool-tip">
+                            ?
+                        </div>
+                    </div>
+                    <textarea class="create-flow-form__input-text-area"></textarea>
+                </div>
+                {/*Edit Name Section*/}
+                <div className="create-flow-form__input">
+                    <div className="create-flow-form__input-label">
+                        <div className="create-flow-form__input-label-text">
+                            <div className="create-flow-form__input-label-icon">&#9998;</div>
+                            EDIT NAME
+                        </div>
+                        <div className="create-flow-form__input-label-text">
+                            <div className="create-flow-form__input-label-icon">&#128247;</div>
+                            AUTHOR PHOTO
+                        </div>
+                        <div className="create-flow-form__input-tool-tip">
+                            ?
+                        </div>
+                    </div>
+                    <textarea className="create-flow-form__input-text-area"></textarea>
+                </div>
+                {/*Edit Target Section*/}
+                <div className="create-flow-form__input">
+                    <div className="create-flow-form__input-label">
+                        <div className="create-flow-form__input-label-text">
+                            <div className="create-flow-form__input-label-icon">&#9998;</div>
+                            EDIT TARGET
+                        </div>
+                        <div className="create-flow-form__input-tool-tip">
+                            ?
+                        </div>
+                    </div>
+                    <textarea className="create-flow-form__input-text-area"></textarea>
+                </div>
+                {/*Edit Summary Section*/}
+                <div className="create-flow-form__input">
+                    <div className="create-flow-form__input-label">
+                        <div className="create-flow-form__input-label-text">
+                            <div className="create-flow-form__input-label-icon">&#9998;</div>
+                            EDIT SUMMARY
+                        </div>
+                        <div className="create-flow-form__input-tool-tip">
+                            ?
+                        </div>
+                    </div>
+                    <textarea className="create-flow-form__input-text-area"></textarea>
                 </div>
             </form>
             <button className="create-flow-form__publish-button">PUBLISH</button>
